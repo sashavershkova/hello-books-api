15191776c681cd2e6d0e9ed5963627271f465d15
sashavershkova <a.m.vershkova@gmail.com>
2025-04-16 19:05:22 -0700

    add books_bp

---
+++ app/models/book.py
@@ -0,0 +1,14 @@
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "The Lord of the Rings", "An epic fantasy saga by J.R.R. Tolkien about the struggle to destroy a powerful ring."),
+    Book(2, "The Wheel of Time", "A long-running fantasy series by Robert Jordan that follows the battle between light and shadow."),
+    Book(3, "A Game of Thrones", "The first book in George R.R. Martin's series about noble families vying for control of a kingdom."),
+    Book(4, "The Name of the Wind", "A lyrical fantasy novel by Patrick Rothfuss about a gifted young man who grows into a legendary figure."),
+    Book(5, "Mistborn: The Final Empire", "A high fantasy heist story by Brandon Sanderson set in a world of ash and oppression.")
+]
