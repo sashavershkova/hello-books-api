fe5117d4bd391b8f644c61d0a990779348ca932c
sashavershkova <a.m.vershkova@gmail.com>
2025-04-16 22:04:05 -0700

    add error handling to get_one_book

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -18,14 +18,17 @@ def get_all_books():
 
 @books_bp.get("/<book_id>")
 def get_one_book(book_id):
-    book_id = int(book_id)
-    book_response = {}
+    try:
+        book_id = int(book_id)
+    except:
+        return {"message": f"book {book_id} invalid"}, 400
+    
     for book in books:
         if book.id == book_id:
-            book_response = {
+            return {
                 "id": book.id,
                 "title": book.title,
-                "description": book.description
+                "description": book.description,
             }
-    return book_response
+    return {"message": f"book {book_id} not found"}, 404
 
