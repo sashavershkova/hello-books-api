eafe81ee99bc4718578bff500f68a53145f835c2
sashavershkova <a.m.vershkova@gmail.com>
2025-04-26 13:57:30 -0700

    add update, delete, read one book endpoints

--- app/models/book.py
+++ app/models/book.py
@@ -1,14 +1,22 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
 
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
 
-books = [
-    Book(1, "The Lord of the Rings", "An epic fantasy saga by J.R.R. Tolkien about the struggle to destroy a powerful ring."),
-    Book(2, "The Wheel of Time", "A long-running fantasy series by Robert Jordan that follows the battle between light and shadow."),
-    Book(3, "A Game of Thrones", "The first book in George R.R. Martin's series about noble families vying for control of a kingdom."),
-    Book(4, "The Name of the Wind", "A lyrical fantasy novel by Patrick Rothfuss about a gifted young man who grows into a legendary figure."),
-    Book(5, "Mistborn: The Final Empire", "A high fantasy heist story by Brandon Sanderson set in a world of ash and oppression.")
-]
+    
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
+
+# books = [
+#     Book(1, "The Lord of the Rings", "An epic fantasy saga by J.R.R. Tolkien about the struggle to destroy a powerful ring."),
+#     Book(2, "The Wheel of Time", "A long-running fantasy series by Robert Jordan that follows the battle between light and shadow."),
+#     Book(3, "A Game of Thrones", "The first book in George R.R. Martin's series about noble families vying for control of a kingdom."),
+#     Book(4, "The Name of the Wind", "A lyrical fantasy novel by Patrick Rothfuss about a gifted young man who grows into a legendary figure."),
+#     Book(5, "Mistborn: The Final Empire", "A high fantasy heist story by Brandon Sanderson set in a world of ash and oppression.")
+# ]
--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,10 +1,46 @@
-from flask import Blueprint, abort, make_response
-from app.models.book import books
+from flask import Blueprint, abort, make_response, request, Response
+from app.models.book import Book
+from ..db import db
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
+# POST ONE BOOK
+@books_bp.post("")
+def create_book():
+    request_body = request.get_json()
+    title = request_body["title"]
+    description = request_body["description"]
+    # checking the content of request_body itself is redundant 
+    # because constructor of a class do it, but checking a legitimate keys makes sense
+    if not title.strip() or not description.strip():
+        response = {"message": "Invalid request. Both title and description must be provided."}
+        abort(make_response(response, 400))
+
+    # Check for duplicates
+    existing_book = Book.query.filter_by(title=title, description=description).first()
+    if existing_book:
+        response = {"message": "This book already exists in the database."}
+        abort(make_response(response, 400))
+
+
+    new_book = Book(title=title, description=description)
+    db.session.add(new_book)
+    db.session.commit()
+
+    response = {
+        "id": new_book.id,
+        "title": new_book.title,
+        "description": new_book.description,
+    }
+
+    return response, 201
+
+# GET ALL BOOKS
 @books_bp.get("")
 def get_all_books():
+    query = db.select(Book).order_by(Book.id)
+    books = db.session.scalars(query)
+
     books_response = []
     for book in books:
         books_response.append(
@@ -16,6 +52,7 @@ def get_all_books():
         )
     return books_response
 
+# GET ONE BOOK
 @books_bp.get("/<book_id>")
 def get_one_book(book_id):
     book = validate_book(book_id)
@@ -26,6 +63,30 @@ def get_one_book(book_id):
         "description": book.description,
     }
 
+# UPDATE A BOOK
+@books_bp.put("/<book_id>")
+def update_one_book(book_id):
+    book = validate_book(book_id)
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+# DELETE ONE BOOK
+@books_bp.delete("/<book_id>")
+def delete_one_book(book_id):
+    book = validate_book(book_id)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+
+# HELPER FUNCTION VALIDATING A BOOK
 def validate_book(book_id):
     try:
         book_id = int(book_id)
@@ -33,10 +94,14 @@ def validate_book(book_id):
         response = {"message": f"book {book_id} invalid"}
         abort(make_response(response, 400))
 
-    for book in books:
-        if book.id == book_id:
-            return book
+    query = db.select(Book).where(Book.id == book_id)
+    book = db.session.scalar(query)
 
-    response = {"message": f"book {book_id} not found"}
-    abort(make_response(response, 404))
+    if not book:
+        response = {"message": f"book {book_id} not found"}
+        abort(make_response(response, 404))
+
+    return book
+
+
 
